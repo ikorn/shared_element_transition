7bd6baf5fba70614ac518a6a5ad127b166356274
ikorn <ikorn@mac.com>
2018-11-28 22:34:57 +0900

    add half size

--- app/src/main/java/com/ikorn/transition/HomeFragment.kt
+++ app/src/main/java/com/ikorn/transition/HomeFragment.kt
@@ -1,12 +1,15 @@
 package com.ikorn.transition
 
 
+import android.content.Context
 import android.os.Bundle
+import android.util.DisplayMetrics
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.WindowManager
 import androidx.core.os.bundleOf
 import androidx.core.view.ViewCompat
 import androidx.databinding.DataBindingUtil
@@ -16,6 +19,7 @@ import androidx.recyclerview.widget.PagerSnapHelper
 import androidx.recyclerview.widget.RecyclerView
 import com.ikorn.transition.databinding.FragmentHomeBinding
 import com.ikorn.transition.databinding.ItemCardBinding
+import com.ikorn.transition.databinding.ItemCardHalfBinding
 import java.io.Serializable
 
 class HomeFragment : Fragment() {
@@ -33,16 +37,16 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         postponeEnterTransition()
-        val recyclerAdapter = HomeContentAdapter(this::onClick)
+        val contentAdapter = HomeContentAdapter(false, this::onClick)
         with(binding.contents) {
             setHasFixedSize(true)
             PagerSnapHelper().attachToRecyclerView(this)
-            adapter = recyclerAdapter
+            adapter = contentAdapter
             viewTreeObserver.addOnGlobalLayoutListener {
                 startPostponedEnterTransition()
             }
         }
-        recyclerAdapter.data = data.mapIndexed { index, title -> CardItem(index, title) }
+        contentAdapter.data = data.mapIndexed { index, title -> CardItem(index, title) }
     }
 
     private fun onClick(item: CardItem, map: Map<String, View>) =
@@ -59,7 +63,7 @@ data class CardItem(private val position: Int, val title: String) : Serializable
     val titleName: String get() = "title_$position"
 }
 
-class HomeContentAdapter(val onClick: (CardItem, Map<String, View>) -> Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class HomeContentAdapter(val half: Boolean, private val onClick: (CardItem, Map<String, View>) -> Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
     var data: List<CardItem>? = null
         set(value) {
@@ -67,11 +71,22 @@ class HomeContentAdapter(val onClick: (CardItem, Map<String, View>) -> Unit) : R
             notifyDataSetChanged()
         }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder = ContentVH(parent)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder =
+            if (half) HalfContentVH(parent)
+            else ContentVH(parent)
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        with((holder as ContentVH)) {
-            val item = data?.get(position) ?: return
+        val item = data?.get(position) ?: return
+        if (half) (holder as HalfContentVH).bind(item, onClick)
+        else (holder as ContentVH).bind(item, onClick)
+    }
+
+    override fun getItemCount(): Int = data?.size ?: 0
+
+    inner class ContentVH(private val binding: ItemCardBinding) : RecyclerView.ViewHolder(binding.root) {
+        constructor(parent: ViewGroup) : this(DataBindingUtil.inflate<ItemCardBinding>(LayoutInflater.from(parent.context), R.layout.item_card, parent, false))
+
+        fun bind(item: CardItem, onClick: (CardItem, Map<String, View>) -> Unit) {
             binding.item = item
             ViewCompat.setTransitionName(binding.card, item.cardName)
             ViewCompat.setTransitionName(binding.title, item.titleName)
@@ -81,9 +96,29 @@ class HomeContentAdapter(val onClick: (CardItem, Map<String, View>) -> Unit) : R
         }
     }
 
-    override fun getItemCount(): Int = data?.size ?: 0
+    inner class HalfContentVH(private val binding: ItemCardHalfBinding) : RecyclerView.ViewHolder(binding.root) {
+        constructor(parent: ViewGroup) : this(DataBindingUtil.inflate<ItemCardHalfBinding>(LayoutInflater.from(parent.context), R.layout.item_card_half, parent, false))
 
-    inner class ContentVH(val binding: ItemCardBinding) : RecyclerView.ViewHolder(binding.root) {
-        constructor(parent: ViewGroup) : this(DataBindingUtil.inflate<ItemCardBinding>(LayoutInflater.from(parent.context), R.layout.item_card, parent, false))
+        init {
+            val displayMetrics = DisplayMetrics()
+            (itemView.context.getSystemService(Context.WINDOW_SERVICE) as WindowManager)
+                    .defaultDisplay
+                    .getMetrics(displayMetrics)
+            val width = displayMetrics.widthPixels
+            val params = RecyclerView.LayoutParams(width / 2, RecyclerView.LayoutParams.MATCH_PARENT)
+            val margin8 = itemView.resources.getDimensionPixelSize(R.dimen.margin_8dp)
+            params.setMargins(margin8, margin8, margin8, margin8)
+            itemView.layoutParams = params
+        }
+
+        fun bind(item: CardItem, onClick: (CardItem, Map<String, View>) -> Unit) {
+            binding.item = item
+            ViewCompat.setTransitionName(binding.card, item.cardName)
+            ViewCompat.setTransitionName(binding.title, item.titleName)
+            binding.card.setOnClickListener {
+                onClick(item, mapOf(item.cardName to binding.card, item.titleName to binding.title))
+            }
+        }
+
     }
 }
